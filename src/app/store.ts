e5d888f6eaca10ddedcd764c26deca6c6e84da83
kyz91994 <kyz91994@mail.ru>
2022-06-09 17:48:18 +0300

    Initial commit

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import thunkMiddleware from 'redux-thunk'
+import {configureStore} from '@reduxjs/toolkit'
+import {rootReducer} from './reducers'
+
+
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+})
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('./reducers', () => {
+        store.replaceReducer(rootReducer)
+    })
+}
