12da4363e3e1547de01348011272d31f204b716e
kyz91994 <kyz91994@mail.ru>
2022-06-09 17:53:23 +0300

    Updates

---
+++ precache-manifest.8f9d4e687716d767ca1ac05e08e414ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a258e18059ec556f63140f692b5b28d",
+    "url": "/todolists-app/index.html"
+  },
+  {
+    "revision": "1bd925ae21c53092bf5f",
+    "url": "/todolists-app/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "031b53254be0fea8fe8a",
+    "url": "/todolists-app/static/js/2.a22f0000.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolists-app/static/js/2.a22f0000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd925ae21c53092bf5f",
+    "url": "/todolists-app/static/js/main.0e7ce5eb.chunk.js"
+  },
+  {
+    "revision": "1c7dac9efb9db99d5acc",
+    "url": "/todolists-app/static/js/runtime-main.9f08e9ac.js"
+  }
+]);
